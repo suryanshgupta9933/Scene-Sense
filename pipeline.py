76b250c2958c0274250a92cc88b38ae308b86a30
Suryansh Gupta <suryanshgupta9933@gmail.com>
2023-06-30 18:01:40 +0530

    rough pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,12 +1,17 @@
 # Importing Dependencies
 import torch
 import torch.nn.functional as F
-from PIL import Image
 import open_clip
 import os
-import numpy as np
-import sqlite3
 from typing import List
+from pymongo import MongoClient
+from PIL import Image
+import nltk
+from nltk.corpus import stopwords
+
+# Download NLTK stopwords and set up
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
 
 # Check if GPU is available and set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -15,21 +20,22 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k', device=device)
 tokenizer = open_clip.get_tokenizer('ViT-B-32')
 
-# Set up SQLite database
-conn = sqlite3.connect('image_embeddings.db')
-c = conn.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS embeddings (image_path TEXT, embedding BLOB)')
+# Create a MongoDB client
+client = MongoClient('mongodb://localhost:27017')  # replace with your connection string
 
-# Function to get embeddings from image and store in SQLite DB
+# Create a database and a collection within the database
+db = client['scene-sense']
+embeddings_collection = db['sample-embeddings']
+
+# Function to get embeddings from image and store in MongoDB
 def process_image_dir(directory: str):
     for filename in os.listdir(directory):
         if filename.endswith(".jpg") or filename.endswith(".png"):
             image_path = os.path.join(directory, filename)
             
             # Check if the path already exists in the database
-            c.execute("SELECT * FROM embeddings WHERE image_path=?", (image_path,))
-            data=c.fetchone()
-            if data is not None:
+            existing_document = embeddings_collection.find_one({'image_path': image_path})
+            if existing_document is not None:
                 # If the path already exists in the database, skip this image
                 continue
                 
@@ -38,12 +44,17 @@ def process_image_dir(directory: str):
             with torch.no_grad(), torch.cuda.amp.autocast():
                 image_features = model.encode_image(image)
                 image_features /= image_features.norm(dim=-1, keepdim=True)
-            # Save to SQLite
-            c.execute("INSERT INTO embeddings VALUES (?, ?)", (image_path, image_features.cpu().numpy().tobytes()))
-    conn.commit()
+            
+            # Save to MongoDB
+            document = {
+                'image_path': image_path,
+                'embedding': image_features.cpu().numpy().tolist()  # convert tensor to list for storage
+            }
+            embeddings_collection.insert_one(document)
 
+from IPython.display import display, Image
 # Search for similar images given a query in DB
-def run_query(query: str, top_k: int = 5) -> List[str]:
+def run_query(query: str, similarity_threshold: float = 0.22) -> List[str]:
     # Tokenize and encode the query
     text = tokenizer([query]).to(device)
     with torch.no_grad(), torch.cuda.amp.autocast():
@@ -51,20 +62,35 @@ def run_query(query: str, top_k: int = 5) -> List[str]:
         text_features /= text_features.norm(dim=-1, keepdim=True)
 
     # Get all embeddings from the database
-    c.execute("SELECT * FROM embeddings")
-    rows = c.fetchall()
+    documents = embeddings_collection.find()
 
-    # Compute similarities with all image embeddings and get top k
-    similarities = []
-    for row in rows:
-        image_path, embedding_bytes = row
-        print(len(np.frombuffer(embedding_bytes, dtype=np.float32)))
-        image_features = torch.from_numpy(np.frombuffer(embedding_bytes, dtype=np.float32)).to(device)
-        print(f"text_features.shape: {text_features.shape}, image_features.shape: {image_features.shape}")  # Debug print
-        similarity = F.cosine_similarity(text_features, image_features.unsqueeze(0))
-        similarities.append((similarity.item(), image_path))
+    # Compute similarities with all image embeddings and get all that are above the threshold
+    similar_images = []
+    for document in documents:
+        image_path = document['image_path']
+        image_features = torch.tensor(document['embedding'], device=device)
+        similarity = F.cosine_similarity(text_features, image_features)
 
-    # Get top k results
-    top_results = sorted(similarities, key=lambda x: x[0], reverse=True)[:top_k]
-    for sim, path in top_results:
+        if similarity.item() > similarity_threshold:
+            similar_images.append((similarity.item(), image_path))
+
+    # Sort by similarity score
+    similar_images = sorted(similar_images, key=lambda x: x[0], reverse=True)
+    
+    # Print and display images
+    for sim, path in similar_images:
         print(f"Similarity: {sim}, Image Path: {path}")
+        display(Image(filename=path))
+
+def compute_similarity_threshold(prompt: str, base_threshold: float = 0.23, increment: float = 0.015) -> float:
+    words = [word for word in prompt.split() if word not in stop_words]
+    num_words = len(words)
+    return base_threshold + num_words * increment
+
+# Create Embeddings
+process_image_dir('sample_images')
+
+prompt = ""
+# Search for similar images
+threshold = compute_similarity_threshold(prompt)
+run_query(prompt, threshold)
