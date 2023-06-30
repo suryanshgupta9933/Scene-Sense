5ed060eb3a01ae34b1e14ceb27317c20d1e0b75b
Suryansh Gupta <suryanshgupta9933@gmail.com>
2023-06-30 19:33:06 +0530

    server side fastapi app

--- app.py
+++ app.py
@@ -1,35 +1,50 @@
-from fastapi import FastAPI
+# Importing Dependencies
+from fastapi import FastAPI, UploadFile, File, Body
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 import torch
 import open_clip
 from PIL import Image
 import io
-import base64
 
+# Create your FastAPI instance
 app = FastAPI()
 
+# Define your data model
+class Text(BaseModel):
+    prompt: Optional[str] = None
+
+# Check if GPU is available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# Load the model and tokenizer
 model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k', device=device)
 tokenizer = open_clip.get_tokenizer('ViT-B-32')
 
-class ImageData(BaseModel):
-    images: List[str] # list of base64 encoded image strings
+# Image Embeddings Endpoint
+@app.post("/image_embeddings/")
+async def create_embeddings(images: List[UploadFile] = File(...)):
+    embeddings = []
 
-@app.post("/")
-async def create_embeddings(image_data: ImageData):
-    images = []
-
-    for base64_image in image_data.images:
-        image_bytes = base64.b64decode(base64_image)
+    for image in images:
+        image_bytes = await image.read()
         image = Image.open(io.BytesIO(image_bytes))
-        images.append(preprocess(image))
+        image = preprocess(image).unsqueeze(0).to(device)
 
-    images = torch.stack(images).to(device)
+        with torch.no_grad(), torch.cuda.amp.autocast():
+            image_features = model.encode_image(image)
+
+        embeddings.append(image_features.tolist())
+
+    return {"image_embeddings": embeddings}
+
+# Text Embeddings Endpoint
+@app.post("/text_embeddings/")
+async def create_text_embeddings(item: Text):
+    text = tokenizer(item.prompt).to(device)
 
     with torch.no_grad(), torch.cuda.amp.autocast():
-        image_features = model.encode_image(images)
+        text_features = model.encode_text(text)
+        text_features /= text_features.norm(dim=-1, keepdim=True)
 
-    # Convert features to list of lists and return
-    return image_features.tolist()
+    return {"text_embedding": text_features.tolist()}
