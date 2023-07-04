6b851bbcb4a8d6e7faad97e2855ef71eeaa9f754
Suryansh Gupta <suryanshgupta9933@gmail.com>
2023-07-04 12:46:47 +0530

    url instead of image

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# Importing Dependencies
-from fastapi import FastAPI, UploadFile, File, Body
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, HttpUrl
 from typing import List, Optional
 import torch
 import open_clip
 from PIL import Image
 import io
+import requests
 
 # Create your FastAPI instance
 app = FastAPI()
@@ -13,6 +13,9 @@ app = FastAPI()
 # Define your data model
 class Text(BaseModel):
     prompt: Optional[str] = None
+
+class ImageUrl(BaseModel):
+    url: HttpUrl
 
 # Check if GPU is available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -23,11 +26,19 @@ tokenizer = open_clip.get_tokenizer('ViT-B-32')
 
 # Image Embeddings Endpoint
 @app.post("/image_embeddings/")
-async def create_embeddings(images: List[UploadFile] = File(...)):
+async def create_embeddings(image_urls: List[ImageUrl]):
     embeddings = []
 
-    for image in images:
-        image_bytes = await image.read()
+    for image_url in image_urls:
+        try:
+            response = requests.get(image_url.url)
+            response.raise_for_status()
+        except requests.HTTPError as http_err:
+            raise HTTPException(status_code=400, detail=f"HTTP error occurred: {http_err}")
+        except Exception as err:
+            raise HTTPException(status_code=400, detail=f"Error occurred: {err}")
+
+        image_bytes = response.content
         image = Image.open(io.BytesIO(image_bytes))
         image = preprocess(image).unsqueeze(0).to(device)
 
