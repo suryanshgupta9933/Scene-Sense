4b821fd544cd5dbd72ede61e0e32cb2f5eeb5e77
Suryansh Gupta <suryanshgupta9933@gmail.com>
2024-07-22 20:39:59 +0530

    streamlit app interface

--- app.py
+++ app.py
@@ -1,120 +1,43 @@
-# Importing Dependencies
-import os
-import requests
-from PIL import Image
+# Install Dependencies
 import streamlit as st
-from dotenv import load_dotenv
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
-from utils import send_text, compute_similarity_threshold, get_similar_images
-from math import ceil
+import requests
 
-# Loading Environment Variables
-load_dotenv()
-username = os.getenv("MONGO_USERNAME")
-password = os.getenv("MONGO_PASSWORD")
+from app_elements import show_home_page, show_login_page, show_signup_page
 
-# Username and password for MongoDB Atlas
-uri = f'mongodb+srv://{username}:{password}@scene-sense.9km2ony.mongodb.net/?retryWrites=true&w=majority'
-
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-# Your database name
-db = client['scene-sense']
-
-# Your collection name
-embeddings_collection = db['embeddings']
-
-# Page config
+# Set Page Config
 st.set_page_config(
     page_title="Scene Sense",
-    page_icon="https://pbs.twimg.com/profile_images/1662946160326352897/wcFtvNCi_400x400.png",  # replace with URL of your icon
-    layout="wide"  # enables a wider screen layout
+    page_icon="https://pbs.twimg.com/profile_images/1662946160326352897/wcFtvNCi_400x400.png",
 )
 
-@st.cache_resource()
-def get_images_from_db():
-    # Get all documents from the database
-    documents = list(embeddings_collection.find())
-    return documents
+# Initialize session state
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
 
-def display_images_from_db():
-    # Get all documents from the database
-    documents = get_images_from_db()
+if "username" not in st.session_state:
+    st.session_state.username = ""
 
-    # Number of images per page
-    images_per_page = 50
+# Title and description
+st.title('🖼️ Scene Sense')
 
-    # Compute number of pages
-    num_pages = ceil(len(documents) / images_per_page)
+if st.session_state.logged_in:
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Choose an option", ["Home", "Logout"])
+    
+    if page == "Home":
+        st.subheader(f"Hello, {st.session_state.username.upper()}!")
+    elif page == "Logout":
+        st.session_state.logged_in = False
+        st.session_state.username = ""
+        st.rerun()
 
-    # Add a selectbox for page number
-    page = st.sidebar.selectbox("Page", list(range(1, num_pages + 1)))
-
-    # Filter documents for the current page
-    start = (page - 1) * images_per_page
-    end = start + images_per_page
-    documents_page = documents[start:end]
-
-    # Creating a fixed number of columns for gallery view
-    num_columns = 4
-    cols = st.columns(num_columns)
-
-    for index, document in enumerate(documents_page):
-        image_url = document['url']
-        image = Image.open(requests.get(image_url, stream=True).raw)
-
-        with cols[index % num_columns]:  # Cycling through columns
-            st.image(image)
-
-def app():
-    # Initialize session state
-    if 'mode' not in st.session_state:
-        st.session_state['mode'] = "Search"
-
-    # App title and explanation
-    st.title('🎠Scene Sense')
-    st.write('Scene Sense is an AI-powered reverse search engine gallery app that revolutionizes the way users interact with their photo collections.')
-    st.write('With Scene Sense, finding the perfect photo becomes a breeze as users can search using natural language queries.')
-
-    # Sidebar with mode selection
-    st.sidebar.title("Mode")
-    mode = st.sidebar.selectbox("Choose a mode", options=["Search", "Gallery View"], index=0 if st.session_state['mode'] == "Search" else 1)
-
-    # Update mode in session state
-    st.session_state['mode'] = mode
-
-    if mode == "Search":
-        # Prompt for search
-        st.subheader("Search for Images")
-        st.markdown('For example:\n- "sunset"\n- "dog wrapped in a blanket"')
-        prompt = st.text_input('Enter a search prompt:')
-        if st.button('Find Similar Images'):
-            text_embedding = send_text(prompt)
-            if text_embedding is not None:
-                text_embedding = text_embedding.get('text_embedding')
-                similarity_threshold = compute_similarity_threshold(prompt)
-                similar_images = get_similar_images(text_embedding, similarity_threshold)
-                if similar_images:
-                    st.success('Similar images found!')
-                    # Display similar images
-                    st.subheader("Similar Images")
-                    num_columns = 4
-                    cols = st.columns(num_columns)
-                    for index, image_url in enumerate(similar_images):
-                        image = Image.open(requests.get(image_url, stream=True).raw)
-                        with cols[index % num_columns]:
-                            st.image(image)
-                else:
-                    st.info('No similar images found.')
-            else:
-                st.error('Error processing the prompt.')
-    elif mode == "Gallery View":
-        # Display images from the database
-        st.subheader("Gallery View")
-        st.markdown('These are 800 sample images from unsplash used for showcasing the demo.')
-        display_images_from_db()
-
-if __name__ == "__main__":
-    app()
+else:
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Choose an option", ["Home", "Login", "Signup"])
+    
+    if page == "Home":
+        show_home_page()
+    elif page == "Login":
+        show_login_page()
+    elif page == "Signup":
+        show_signup_page()
---
+++ app_elements.py
@@ -0,0 +1,46 @@
+# Install Dependencies
+import streamlit as st
+import requests
+
+# Navigation
+def show_home_page():
+    st.markdown("""
+        Scene Sense is an AI-powered image search engine gallery app that revolutionizes the way users interact with their photo collections. 
+        \nWith Scene Sense, finding the perfect photo becomes a breeze as users can search using natural language queries.
+    """)
+    st.subheader("Welcome to Scene Sense!")
+    st.write("Explore and search your photo collection seamlessly with AI-powered technology.")
+    st.image("https://weandthecolor.com/wp-content/uploads/2022/10/Download-Minimalist-Collage-Art-by-Adobe-Stock-Contributor-Irina.jpg", use_column_width=True)
+
+def show_login_page():
+    st.subheader("Login")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        response = requests.post("http://localhost:8000/token", data={"username": username, "password": password})
+        if response.status_code == 200:
+            token = response.json()["access_token"]
+            response = requests.get("http://localhost:8000/login", headers={"Authorization": f"Bearer {token}"})
+            if response.status_code == 200:
+                st.session_state.logged_in = True
+                st.session_state.username = response.json()["username"]
+                st.rerun()
+            else:
+                st.error(response.json().get("detail", "Failed to fetch user details"))
+        else:
+            st.error(response.json().get("detail", "Incorrect username or password"))
+
+def show_signup_page():
+    st.subheader("Signup")
+    new_username = st.text_input("Username")
+    new_password = st.text_input("Password", type="password")
+    confirm_password = st.text_input("Confirm Password", type="password")
+    if st.button("Signup"):
+        if new_password == confirm_password:
+            response = requests.post("http://localhost:8000/signup", json={"username": new_username, "password": new_password})
+            if response.status_code == 200:
+                st.success(response.json()["message"])
+            else:
+                st.error(response.json().get("detail", "Failed to register user"))
+        else:
+            st.error("Passwords do not match")
