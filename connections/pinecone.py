c4d2d2a89811ae4e74f8f02cc9880141df0d17ce
Suryansh Gupta <suryanshgupta9933@gmail.com>
2024-07-24 03:20:59 +0530

    Add: pinecone index connection

---
+++ connections/pinecone.py
@@ -0,0 +1,24 @@
+# Importing Dependencies
+import os
+import logging
+from dotenv import load_dotenv
+from pinecone import Pinecone
+
+# Load Environment Variables
+load_dotenv()
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+INDEX_NAME = os.getenv("INDEX_NAME")
+
+# Configure Logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+# Initialize Pinecone
+def connect_pinecone():
+    try:
+        pinecone = Pinecone(api_key=PINECONE_API_KEY)
+        index = pinecone.index(INDEX_NAME)
+        return index
+    except Exception as e:
+        logging.error(f"Failed to connect to Pinecone: {e}")
+        return None
