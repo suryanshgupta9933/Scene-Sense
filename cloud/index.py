cc58d900177be6d0a4a1f7322e8632c12594d3ad
Suryansh Gupta <suryanshgupta9933@gmail.com>
2024-07-26 22:17:01 +0530

    pinecone operations

---
+++ cloud/index.py
@@ -0,0 +1,60 @@
+# Importing Dependencies
+import os
+import logging
+
+from connections.pinecone import connect_pinecone
+from helper import return_user_id, return_embedding_id, return_date, return_time, return_filename
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+def update_index(embeddings):
+    try:
+        # Connect to Pinecone
+        index = connect_pinecone()
+        user_id = return_user_id(embeddings[0]["url"])
+        # Prepare the data
+        embedding_data = []
+        for data in embeddings:
+            embedding_data.append({
+                "id": return_id(data["url"]),
+                "embedding": data["embedding"],
+                "metadata": {
+                    "filename": return_filename(data["url"]),
+                    "url": data["url"],
+                    "date": return_date(data["url"]),
+                    "time": return_time(data["url"]),
+                }    
+            })
+
+        # Update the index
+        index.upsert(
+            vectors=embedding_data,
+            namespace=user_id
+        )
+        logger.info(f"Successfully updated the index for user: {user_id}")
+    except Exception as e:
+        logger.error(f"Failed to update the index: {e}")
+        return None
+
+def query_index(query_embedding, user_id):
+    try:
+        # Connect to Pinecone
+        index = connect_pinecone()
+        
+        # Query the index
+        results = index.query(
+            vector=query_embedding,
+            top_k=20,
+            namespace=user_id
+        )
+        logger.info(f"Successfully queried the index for user: {user_id}")
+
+        # Extract URLs from the matches
+        urls = [match["metadata"]["url"] for match in results["matches"]]
+        
+        return urls
+    except Exception as e:
+        logger.error(f"Failed to query the index: {e}")
+        return None
