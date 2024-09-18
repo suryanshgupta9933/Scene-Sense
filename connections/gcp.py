5f2e2f5880f3b7eaedff8e4549f72eebbb7b8c7b
Suryansh Gupta <suryanshgupta9933@gmail.com>
2024-09-18 21:15:05 +0530

    fixed the gallery not showing on streamlit app

--- connections/gcp.py
+++ connections/gcp.py
@@ -1,5 +1,7 @@
 # Importing Dependencies
 import os
+import json
+import base64
 import logging
 from dotenv import load_dotenv
 from google.cloud import storage
@@ -17,10 +19,12 @@ logger = logging.getLogger(__name__)
 # Initialize the Google Cloud Storage client
 def connect_gcp():
     try:
-        if os.getenv("ENV") == "dev":
-            storage_client = storage.Client.from_service_account_json('scene-sense-9933-190032d295c7.json')
+        if os.getenv("ENV") == "streamlit":
+            gcp_key = os.getenv("GCP_KEY")
+            gcp_key = json.loads(base64.b64decode(gcp_key).decode('utf-8'))
+            storage_client = storage.Client.from_service_account_info(gcp_key)
         else:
-            storage_client = storage.Client()
+            storage_client = storage.Client.from_service_account_json('scene-sense-9933-190032d295c7.json')
         bucket_name = BUCKET_NAME
         bucket = storage_client.get_bucket(bucket_name)
         return bucket
